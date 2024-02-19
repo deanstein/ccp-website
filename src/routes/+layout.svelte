0cdb2f931688d57a8aad6c72cd85bbcc84a5442b
deanstein <jdeangoldstein@gmail.com>
2024-02-19 11:27:36 -0700

    Updating to jdg-ui-svelte v0.1.2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,29 @@
 	import { css } from '@emotion/css';
 	import { JDGBackground, JDGFooter, JDGHeader } from 'jdg-ui-svelte';
 
+	import navItem from 'jdg-ui-svelte/schemas/nav-item.js';
+
 	import { ccpWebsiteRepoName } from 'jdg-ui-svelte/jdg-persistence-management.js';
+
+	import { instantiateObject } from 'jdg-ui-svelte/jdg-utils.js';
+
 	import 'jdg-ui-svelte/jdg-styling-root.css';
 	import { jdgColors } from 'jdg-ui-svelte/jdg-styling-constants.js';
+
+	// define the nav items in the header
+	const newNavItem1 = instantiateObject(navItem);
+	newNavItem1.label = 'HOME';
+	newNavItem1.href = '/';
+
+	const newNavItem2 = instantiateObject(navItem);
+	newNavItem2.label = 'ABOUT';
+	newNavItem2.href = '/about';
+
+	const newNavItem3 = instantiateObject(navItem);
+	newNavItem3.label = 'CONTACT';
+	newNavItem3.href = '/contact';
+
+	const navItems = [newNavItem1, newNavItem2, newNavItem3];
 
 	// get versions from package.json to display in the footer
 	//@ts-expect-error
@@ -18,18 +38,28 @@
 		'Photographs are from public domain sources or are used with permission and credited accordingly. See a photo with missing or wrong credit? Let us know.';
 
 	// global styles, but using emotion css
-	const jdgLayoutCss = css`
+	const ccpLayoutCss = css`
 		a {
 			color: ${jdgColors.text};
 		}
+		a.no-initial-highlight::before,
+		.jdg-highlight-container .jdg-highlight::before {
+			background: ${jdgColors.accentStripesJDG[0]};
+		}
 		a:before {
-			background-color: ${jdgColors.accentStripesJDG[2]};
+			background-color: ${jdgColors.accentStripesJDG[0]};
 		}
 	`;
 </script>
 
-<div class="jdg-layout {jdgLayoutCss}">
-	<JDGHeader logoTitle={'THE CINDERELLA CITY PROJECT'} logoSrc={'./ccp-logo.png'} useMobileNav={true}/>
+<div class="ccp-layout {ccpLayoutCss}">
+	<JDGHeader
+		logoTitle={'THE CINDERELLA CITY PROJECT'}
+		logoSrc={'./ccp-logo.png'}
+		logoJustification="center"
+		{navItems}
+		useMobileNav={true}
+	/>
 
 	<JDGBackground />
 
@@ -39,7 +69,7 @@
 </div>
 
 <style>
-	.jdg-layout {
+	.ccp-layout {
 		display: flex;
 		flex-direction: column;
 	}
