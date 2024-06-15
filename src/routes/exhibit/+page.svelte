ddd6455a4d71dfdf25e44c9bc14a3bfeb2415b19
deanstein <jdeangoldstein@gmail.com>
2024-06-15 00:29:13 -0600

    Updating exhibit page

--- src/routes/exhibit/+page.svelte
+++ src/routes/exhibit/+page.svelte
@@ -1,20 +1,52 @@
 <script>
-	import { JDGBodyCopy, JDGContentBoxFloating, JDGContentContainer, JDGImage } from 'jdg-ui-svelte';
+	import {
+		JDGBodyCopy,
+		JDGContentBoxFloating,
+		JDGContentContainer,
+		JDGImage,
+		JDGImageFullWidth
+	} from 'jdg-ui-svelte';
 	import imageAttributesCollection from '../../image-attributes-collection';
+	import Layout from '../+layout.svelte';
 </script>
 
-<JDGContentContainer>
-	<JDGContentBoxFloating title="COMING SOON">
-		<JDGBodyCopy paddingTop="0">
-			<span style="display: flex; justify-content: center;">
-				This page is under construction. Check back soon!
-			</span>
+<JDGContentContainer overlapWithHeader={true} topBottomPadding="0" gap="0">
+	<JDGImageFullWidth
+		imageAttributes={imageAttributesCollection.ccp_exhibit_composite}
+		showOverlay={true}
+		overlayColorRgba="rgba(30, 30, 30, 0.8)"
+		primaryText="EXPERIENCE THE FAIRYTALE"
+		primaryTextFontFamily="Righteous"
+		secondaryText="at the Historic Englewood Museum"
+		secondaryTextFontFamily="Righteous"
+	/>
+	<JDGContentBoxFloating>
+		<JDGBodyCopy>
+			The <b>Cinderella City Project</b> has teamed up with
+			<a href="https://historicenglewood.org/">Historic Englewood</a> to create Englewood's first history museum, bringing to life the story of <a href="./history">Cinderella City Mall</a> as the inaugural exhibit.
+			<br><br>
+			Immerse yourself in Cinderella City's history and design with life-size photos, little-known facts, and several artifacts from the mall, including the iconic Cinder Alley sign which has been restored to its former glory. 
+			<br><br>
+			The Cinderella City exhibit is on now through fall 2024.
 		</JDGBodyCopy>
-		<JDGImage
-			imageAttributes={imageAttributesCollection.construction_1}
-			maxHeight="60vh"
-			showCaption={true}
-			showAttribution={true}
-		/>
+	</JDGContentBoxFloating>
+</JDGContentContainer>
+<JDGContentContainer>
+	<JDGContentBoxFloating title="EXHIBIT INFORMATION">
+		<JDGBodyCopy paddingTop="0">
+			<h3>LOCATION</h3>
+			Catch the Cinderella City exhibit inside the last remaining piece of Cinderella City Mall:
+			<br><br>
+			<b>Historic Englewood museum</b>
+			<br>
+			Englewood Civic Center, 2nd Floor
+			<br>
+			1000 Englewood Parkway
+			<br>
+			Englewood, Colorado 80110
+			<br><br>
+			<h3>HOURS</h3>
+			Saturdays 10am to 2pm MST
+		</JDGBodyCopy>
 	</JDGContentBoxFloating>
 </JDGContentContainer>
