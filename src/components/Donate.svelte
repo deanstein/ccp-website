34f626296c903c8813e94c41c122024a9ed041b2
deanstein <jdeangoldstein@gmail.com>
2024-07-02 23:18:34 -0600

    Adding separate Donate component

---
+++ src/components/Donate.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { urls } from '$lib/strings';
+	import { JDGBodyCopy, JDGContentBoxFloating } from 'jdg-ui-svelte';
+</script>
+
+<JDGContentBoxFloating title="DONATE">
+	<JDGBodyCopy paddingTop="0">
+		Digitally reconstructing a massive shopping center in two time periods is hard work! The
+		Cinderella City Project is self-funded and built by just <a href={urls.jdgLinkedIn}
+			>one person</a
+		>, requiring dedication, time, and resources.
+		<br /><br />
+		Support the project by buying its creator coffee via Ko-Fi!
+	</JDGBodyCopy>
+	<div style="width: 100%; display: flex; justify-content: center;">
+		<iframe
+			id="kofiframe"
+			src="https://ko-fi.com/cinderellacityproject/?hidefeed=true&widget=true&embed=true&preview=true"
+			style="border:none;padding:4px;background:#f9f9f9;"
+			width="400"
+			height="620"
+			title="cinderellacityproject"
+		/>
+	</div>
+	<JDGBodyCopy>
+		Donations will be used to purchase the content, software, and hardware needed to bring
+		Cinderella City Mall back to life as realistically as possible.
+	</JDGBodyCopy>
+</JDGContentBoxFloating>
