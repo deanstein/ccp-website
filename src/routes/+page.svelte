d0f321e87d989767441ae80ac38f378c2cee3101
deanstein <jdeangoldstein@gmail.com>
2024-07-21 11:49:15 -0600

    Updating to jdg-ui-svelte v0.17.1 for GridLayout fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -193,21 +193,17 @@
 			the simulation in reality:
 		</JDGBodyCopy>
 		<JDGGridLayout maxColumns={2}>
-			<div style="display: grid; max-width: fit-content;">
 				<JDGImageCompare
 					imageAttributes1={imageAttributesCollection.ccp_blue_mall_60s70s_1}
 					imageAttributes2={imageAttributesCollection.ccp_blue_mall_80s90s_1}
 					maxHeight="400px"
 					showBlurInUnfilledSpace={false}
 				/>
-			</div>
-			<div style="display: grid; max-width: fit-content;">
 				<JDGImageTile
 					imageAttributes={imageAttributesCollection.ccp_historic_photos_gold_mall_60s70s_1}
 					fillContainer={false}
 					maxHeight="400px"
 				/>
-			</div>
 		</JDGGridLayout>
 
 		<JDGBodyCopy>
