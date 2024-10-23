605cb90a1ab885e9f6f446805775f28dc1ec6ddf
deanstein <jdeangoldstein@gmail.com>
2024-10-22 22:08:36 -0600

    Updating known issues

--- src/routes/simulation/+page.svelte
+++ src/routes/simulation/+page.svelte
@@ -313,14 +313,11 @@
 			The simulation is still under development, so it has a few known issues:
 			<br /><br />
 			<ul>
-				<li>Large areas of both time periods are unfinished</li>
+				<li>Many storefronts in both time periods are blank/unfinished</li>
 				<li>The audio when walking outside the mall is too short and loops too quickly</li>
 				<li>
 					Jumping or using anti-gravity mode while walking outside the mall will play the interior
 					audio unexpectedly
-				</li>
-				<li>
-					Running the Guided Tour for extended periods may result in the player walking into walls
 				</li>
 			</ul>
 			<br />
